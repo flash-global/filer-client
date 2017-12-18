4362709e1c64147106ddd922e9ac65817eaff1c9
PERUVIEN Christophe <cpe@opcoding.eu>
2017-12-18 12:09:01 +0100

    Create exemple of a use case of the uploadBychunks method

--- examples/upload-chunks.php
+++ examples/upload-chunks.php
@@ -13,19 +13,44 @@ $chunkSize = 20 * 1024 * 1024;
 $filer = new Filer([Filer::OPTION_BASEURL => 'http://127.0.0.1:8020']);
 $filer->setTransport(new BasicTransport());
 
-$file = (new File())
-    ->setCategory(File::CATEGORY_IMG)
-    ->setContexts(['test 1' => 'test 1', 'test 2' => 'test 2'])
-    ->setFilename('avatar.png')
-    ->setFile(new SplFileObject(__DIR__ . '/../tests/_data/avatar.png'));
-
 try {
-    echo $filer->uploadByChunks(
+    $file = (new File())
+        ->setCategory(File::CATEGORY_IMG)
+        ->setContexts(['test 1' => 'test 1', 'test 2' => 'test 2'])
+        ->setFilename('avatar.png')
+        ->setFile(new SplFileObject(__DIR__ . '/../tests/_data/avatar.png'));
+
+    $uuid = $filer->uploadByChunks(
         $file,
+        null,
         function (Response $response, $index) {
             var_dump($index);
         }
-    ) . PHP_EOL;
+    );
+    echo $uuid . PHP_EOL;
+
+    $file = (new File())
+        ->setCategory(File::CATEGORY_IMG)
+        ->setContexts(['test 1' => 'test 1', 'test 2' => 'test 2', 'test 3' => 'test 3'])
+        ->setFile(new SplFileObject(__DIR__ . '/../tests/_data/capture.png'))
+        ->setUuid($uuid);
+
+    $uuid = $filer->uploadByChunks(
+        $file,
+        Filer::NEW_REVISION
+    );
+    echo $uuid . PHP_EOL;
+
+    $file = $filer->retrieve($uuid);
+
+    $file->setContexts(['test 1' => 'New value']);
+
+    $uuid = $filer->uploadByChunks(
+        $file
+    );
+
+    echo $uuid . PHP_EOL;
+
 } catch (FilerException $e) {
     echo $e->getMessage() . PHP_EOL;
 } catch (\Exception $e) {
